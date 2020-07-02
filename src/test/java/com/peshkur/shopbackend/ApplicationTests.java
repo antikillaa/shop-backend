f14c9a607f6719b6d48f0f7088d7b4a4f94e920e
Stanislav Peshkur <stanislav@samantree.com>
2020-07-02 15:20:51 -0700

    Initial commit

---
+++ src/test/java/com/peshkur/shopbackend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peshkur.shopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
